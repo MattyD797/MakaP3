257cb22251055f1930ec588349ef283f84f08264
Harold Mills <harold.mills@gmail.com>
2014-01-28 21:30:43 -0500

    Added all commands from "`96 MMRP Commands 1.01" Aardvark command set to `MmrpCommandInterpreter101` module.

--- src/maka/mmrp/MmrpCommandInterpreter101.py
+++ src/maka/mmrp/MmrpCommandInterpreter101.py
@@ -5,7 +5,18 @@ import datetime
 from maka.command.CommandInterpreterError import CommandInterpreterError
 from maka.command.SimpleCommand import SimpleCommand
 from maka.command.SimpleCommandInterpreter import SimpleCommandInterpreter
-from maka.mmrp.MmrpDocument101 import Comment, Fix, Observer, Role, TheoData
+from maka.mmrp.MmrpDocument101 import (
+    Station, Observer, Theodolite, Reference, Role, Start, End, Comment, Pod, Vessel,
+    Environment, StartScan, StartWhaleScan, EndScan, StartVesselScan, EndVesselScan,
+    Orientation, StartFocalSession,
+#    StartPlayback, EndPlayback,
+    EndFocalSession, Confidence,
+    BinocularFix, TheoData, Fix, Sighting, Behavior, BehavioralState, BehaviorsSynchronous,
+    BehaviorsAsynchronous, PodEvent, VesselEvent, Affiliation, Disaffiliation,
+#    SuspectedAffiliation, SuspectedDisaffiliation,
+    FocalPodLost, Lag, DeleteLastEntry,
+    DeleteLastSequence, EyepieceHeight, BubbleCheck, Rebalance, TideHeight, ClosestApproach,
+    SurfacingNumber)
 from maka.util.SerialNumberGenerator import SerialNumberGenerator
 import maka.device.DeviceManager as DeviceManager
 import maka.util.AngleUtils as AngleUtils
@@ -34,20 +45,8 @@ class MmrpCommandInterpreter101(SimpleCommandInterpreter):
         
         
     def _createCommands(self):
-        return dict((c.name, c) for c in [
-            _Observer(self),
-            _Role(self),
-            _Comment(self),
-            _TheoData(self),
-            _PodFix(self),
-            _VesselFix(self),
-            _SpinnerPodFix(self),
-            _TursiopsPodFix(self),
-            _TurtlePodFix(self),
-            _ReferenceFix(self),
-            _BuoyFix(self),
-            _OtherFix(self)
-        ])
+        commands = [c(self) for c in _commandClasses]
+        return dict((c.name, c) for c in commands)
 
 
     def _getCurrentDateAndTime(self):
@@ -90,10 +89,11 @@ class MmrpCommandInterpreter101(SimpleCommandInterpreter):
         except Exception as e:
             raise CommandInterpreterError('Theodolite read failed. ' + str(e))
         
+        
     def _getAndSaveTheodoliteAngles(self):
         v, h = self._getTheodoliteAngles()
-        self._savedDeclination = AngleUtils.radiansToDegrees(v)
-        self._savedAzimuth = AngleUtils.radiansToDegrees(h)
+        self._savedDeclination = AngleUtils.radiansToDegrees(v) if v is not None else v
+        self._savedAzimuth = AngleUtils.radiansToDegrees(h) if h is not None else h
         return self._getSavedTheodoliteAngles()
         
         
@@ -140,11 +140,25 @@ def _getCommentId(obs):
 _Interpreter = MmrpCommandInterpreter101
 
 
-class _Observer(SimpleCommand):
-    observationClass = Observer
-    format = 'observer initials name'
-    
+_commandNum = 0
 
+
+def _command(format, obsClass, bases=(SimpleCommand,), defaultFieldValues=None):
+    
+    global _commandNum
+    name = '_Command{:03d}'.format(_commandNum)
+    _commandNum += 1
+    
+    attrs = {
+        'observationClass': obsClass,
+        'format': format
+    }
+    if defaultFieldValues is not None:
+        attrs['defaultFieldValues'] = defaultFieldValues
+        
+    return type(name, bases, attrs)
+    
+    
 class _Ndt(SimpleCommand):
     defaultFieldValues = {
         'observationNum': _Interpreter._getNextObsNum,
@@ -152,47 +166,225 @@ class _Ndt(SimpleCommand):
     }
     
     
-class _Comment(_Ndt):
-    observationClass = Comment
-    format = 'c text id'
-    defaultFieldValues = { 'id': _Interpreter._getNextCommentId }
-    
-
-class _Role(_Ndt):
-    observationClass = Role
-    format = 'role observer role'
+def _ndt(format, obsClass, defaultFieldValues=None):
+    return _command(format, obsClass, (_Ndt,), defaultFieldValues)
     
     
-class _TheoData(_Ndt):
-    observationClass = TheoData
-    format = 'z'
-    defaultFieldValues = {
-        ('date', 'time'): _Interpreter._getAndSaveCurrentDateAndTime,
-        ('declination', 'azimuth'): _Interpreter._getAndSaveTheodoliteAngles
-    }
-    
-    
-class _Fix(_Ndt):
-    observationClass = Fix
-    defaultFieldValues = {
-        ('date', 'time'): _Interpreter._getSavedDateAndTime,
-        ('declination', 'azimuth'): _Interpreter._getSavedTheodoliteAngles
-    }
-    
-    
-def _fix(className, commandName, objectType):
-    classAttributes = {
-        'format': commandName + ' objectId behavioralState',
-        'defaultFieldValues': {'objectType': objectType}
-    }
-    return type(className, (_Fix,), classAttributes)
+_fixDefaults = [
+    (('date', 'time'), _Interpreter._getSavedDateAndTime),
+    (('declination', 'azimuth'), _Interpreter._getSavedTheodoliteAngles)
+]
 
 
-_PodFix = _fix('_PodFix', 'p', 'Pod')
-_VesselFix = _fix('_VesselFix', 'v', 'Vessel')
-_SpinnerPodFix = _fix('_SpinnerPodFix', 'sp', 'Spinner pod')
-_TursiopsPodFix = _fix('_TursiopsPodFix', 'bn', 'Tursiops pod')
-_TurtlePodFix = _fix('_TurtlePodFix', 'tu', 'Turtle pod')
-_ReferenceFix = _fix('_ReferenceFix', 'r', 'Reference')
-_BuoyFix = _fix('_BuoyFix', 'by', 'Buoy')
-_OtherFix = _fix('_OtherFix', 'os', 'Other')
+def _fix(commandName, objectType):
+    format = commandName + ' objectId behavioralState'
+    defaultFieldValues = dict(_fixDefaults + [('objectType', objectType)])
+    return _ndt(format, Fix, defaultFieldValues)
+
+
+# TODO: Set `code` field to concatenation of `code` argument and `str(podId)`.
+def _beh(code, behavior):
+    format = code + ' individualId podId'
+    defaultFieldValues = {'code': code, 'behavior': behavior}
+    return _ndt(format, Behavior, defaultFieldValues)
+
+
+def _bst(name, state=None):
+    format = name + ' podId'
+    if state is None: state = name
+    return _ndt(format, BehavioralState, {'state': state})
+
+
+def _pev(code, event):
+    return _objectEvent(code, event, 'pod', PodEvent)
+    
+    
+def _objectEvent(code, event, objectName, obsClass):
+    format = code + ' ' + objectName + 'Id'
+    defaultFieldValues = {'code': code, 'event': event}
+    return _ndt(format, obsClass, defaultFieldValues)
+
+
+def _vev(code, event):
+    return _objectEvent(code, event, 'vessel', VesselEvent)
+
+
+def _cpa(name, objectType):
+    format = name + ' objectId podId'
+    return _ndt(format, ClosestApproach, {'objectType': objectType})
+
+
+_commandClasses = [
+
+    # station setup
+    _command('station id name latitudeDegrees latitudeMinutes longitudeDegrees '
+             'longitudeMinutes elevation magneticDeclination', Station),
+    _command('observer initials name', Observer),
+    _command('theodolite id name azimuthOffset declinationOffset', Theodolite),
+    _command('reference id name azimuth', Reference),
+    _ndt('role observer role', Role),
+    
+    # start and end of observations
+    _ndt('start', Start),
+    _ndt('end', End),
+    
+    _ndt('c text id', Comment, { 'id': _Interpreter._getNextCommentId }),
+    
+    # tracked objects
+    _command('pc id numWhales numCalves numSingers', Pod),
+    _ndt('vt id type', Vessel),
+    
+    # scans
+    _ndt('ssc scanId visibility beaufort swellHeight numVessels numPods', StartScan),
+    _ndt('sws', StartWhaleScan),
+    _ndt('esc', EndScan),
+    
+    # focal sessions
+    _ndt(('sfs sessionId podId orientation speed visibility beaufort swellHeight '
+          'numVessels aircraftDisturbance playbackType'), StartFocalSession),
+    # TODO: Either create commands for StartPlayback and EndPlayback observations
+    # or delete the observation types.
+    _ndt('svs', StartVesselScan),
+    _ndt('evs', EndVesselScan),
+    _ndt('efs', EndFocalSession),
+    
+    _ndt('or orientation speed', Orientation),
+    _ndt('env visibility beaufort swellHeight', Environment),
+    _ndt('cnf confidence', Confidence),
+    
+    _ndt('bf objectType objectId reticle azimuth behavioralState', BinocularFix),
+    
+    # theodolite fixes
+    _ndt('z', TheoData, {
+             ('date', 'time'): _Interpreter._getAndSaveCurrentDateAndTime,
+             ('declination', 'azimuth'): _Interpreter._getAndSaveTheodoliteAngles}),
+    _ndt('fx objectType objectId behavioralState', Fix, dict(_fixDefaults)), 
+    _fix('p', 'Pod'),
+    _fix('v', 'Vessel'),
+    _fix('sp', 'Spinner pod'),
+    _fix('bn', 'Tursiops pod'),
+    _fix('tu', 'Turtle pod'),
+    _fix('r', 'Reference'),
+    _fix('by', 'Buoy'),
+    _fix('os', 'Other'),
+    
+    _ndt('st observerId objectType objectId behavioralState', Sighting),
+    
+    # individual behaviors
+    
+    # respiration
+    _beh('fs', 'First surface with no blow'),
+    _beh('f',  'First surface blow'),
+    _beh('nf', 'Not first surfacing'),
+    _beh('b',  'Blow'),
+    _beh('n',  'No blow rise'),
+    _beh('m',  'Missed blow(s)?'),
+
+    # submergence
+    _beh('s',  'Slip under'),
+    _beh('a',  'Peduncle arch'),
+    _beh('d',  'Fluke down dive'),
+    _beh('u',  'Fluke up dive'),
+    _beh('sq', 'Unidentified submergence'),
+
+    # non-respiratory markers
+    _beh('nr', 'Missed non-respiratory behavior(s)?'),
+    _beh('ub', 'Unidentified behavior'),
+    _beh('ms', 'Missed surfacing'),
+
+    # subsurface exhalations
+    _beh('bc', 'Bubble cloud'),
+    _beh('bt', 'Linear bubble trail'),
+
+    # whale vocalizations
+    _beh('tb', 'Trumpet blow'),
+    _beh('sr', 'Singing reported'),
+    _beh('ss', 'Sideslap'),
+
+    # head and leaping behaviors
+    _beh('hr', 'Head rise'),
+    _beh('hl', 'Head lunge'),
+    _beh('mb', 'Motorboating'),
+    _beh('hs', 'Head slap'),
+    _beh('br', 'Breach'),
+    _beh('us', 'Unidentified large splash'),
+    _beh('oh', 'Other head behavior'),
+    _beh('ap', 'Airplane'),
+    _beh('h',  'Helicopter'),
+
+    # tail behaviors
+    _beh('te', 'Tail extension'),
+    _beh('ts', 'Tail slap'),
+    _beh('ls', 'Lateral tail slap'),
+    _beh('sw', 'Tail swish'),
+    _beh('lt', 'Lateral tail display'),
+    _beh('ot', 'Other tail behavior'),
+    
+    # pectoral fin behaviors
+    _beh('pe', 'Pec extension'),
+    _beh('ps', 'Pec slap'),
+    _beh('rp', 'Rolling pec slap'),
+    _beh('op', 'Other pec behavior'),
+
+    # body contact
+    _beh('sb', 'Strike with body part'),
+    _beh('wc', 'Whale body contact'),
+
+    # pod behavioral states
+    _bst('rest'),
+    _bst('mill'),
+    _bst('trav'),
+    _bst('stat'),
+    _bst('sact'),
+    _bst('unkn'),
+    _bst('whalewatch'),
+    _bst('1', 'rest'),
+    _bst('2', 'mill'),
+    _bst('3', 'trav'),
+    _bst('4', 'stat'),
+    _bst('5', 'sact'),
+    _bst('6', 'unkn'),
+    _bst('7', 'whalewatch'),
+    
+    _ndt('sync', BehaviorsSynchronous),
+    _ndt('asyn', BehaviorsAsynchronous),
+    
+    # pod events
+    _pev('pd',   'Pod decreases speed'),
+    _pev('pi',   'Pod increases speed'),
+    _pev('px',   'Pod stops'),
+    _pev('p45',  'Pod changes direction 45 to 90 degrees'),
+    _pev('p90',  'Pod changes direction 90 to 180 degrees'),
+    _pev('p180', 'Pod changes direction 180 degrees'),
+    
+    # vessel events
+    _vev('vs', 'Vessel starts'),
+    _vev('vc', 'Vessel changes speed'),
+    _vev('vx', 'Vessel stops'),
+    
+    _ndt('paf oldPodId1 oldPodId2 newPodId', Affiliation),
+    _ndt('pds oldPodId newPodId1 newPodId2', Disaffiliation),
+    # TODO: Either create commands for the SuspectedAffiliation and SuspectedDisaffiliation
+    # observations or delete the observation types.
+    _ndt('pl', FocalPodLost),
+    
+    # document edits
+    _ndt('l lag', Lag),
+    _ndt('x', DeleteLastEntry),
+    _ndt('xx', DeleteLastSequence),
+    
+    # theodolite
+    _ndt('eh height', EyepieceHeight),
+    _ndt('tbc', BubbleCheck),
+    _ndt('rbt', Rebalance),
+    
+    _ndt('th height', TideHeight),
+    
+    # closest approaches
+    _cpa('cpav', 'Vessel'),
+    _cpa('cpaa', 'Airplane'),
+    _cpa('cpah', 'Helicopter'),
+    
+    _ndt('sn surfacingNum', SurfacingNumber)
+    
+]
